88263117786cfbdab6a2f3f0a55bb55d7ea1ac75
GrandmasRecipes <aichm0782@gmail.com>
2020-03-27 10:47:14 +0100

    Add style and images

---
+++ ExterneDatei.js
@@ -0,0 +1,35 @@
+
+//===================================================Rezepte Array==========================================================
+let rezept0 = {
+	headline: 'Index0',
+	src: 'img/Tomato2.jpg',
+	description: 'Index0 Beschreibung',
+	ingridients: 'Wasser, Mehl',
+	making: 'Index0 Making'
+}
+
+let rezept1 = {
+	headline: 'Hallo2',
+	src: 'img/logo.png',
+	description: 'TestTestBeschreibung2',
+	ingridients: 'Wasser, Mehl2',
+	making: 'TestTestZubereitung2'
+}
+
+let array = [rezept0, rezept1];
+
+
+
+/**
+function saveRecipeIndex(string) {
+	window.name = string;
+}
+
+function loadRecipe() {
+	let index = Number(window.name);
+	
+	document.getElementById('headline').innerHTML = array[index].headline;
+	document.getElementById('image').src = array[index].src;
+}
+**/
+
