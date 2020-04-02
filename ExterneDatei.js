3e50028c04c8f18dda8e96d443793264050b1433
MarvinAichinger <marvin.aichinger@outlook.com>
2020-04-02 08:11:10 +0200

    Complete responsiv nav

--- ExterneDatei.js
+++ ExterneDatei.js
@@ -6,15 +6,37 @@ let RecipesArray = [
 		"src": "Test1",
 		"description": "Test1",
 		"ingredients": "Test1",
-		"making": "Test1"
+		"making": "Test1",
+		"time": "5 min",
+		"gang": "Hauptspeise"
 	},
 	{
 		"headline": "Test2",
 		"src": "Test2",
 		"description": "Test2",
 		"ingredients": "Test2",
-		"making": "Test2"
+		"making": "Test2",
+		"time": "5 min",
+		"gang": "Hauptspeise"
 	},
+	{
+		"headline": "Test3",
+		"src": "Test3",
+		"description": "Test3",
+		"ingredients": "Test3",
+		"making": "Test3",
+		"time": "Test3",
+		"gang": "Test3"
+	},
+	{
+		"headline": "Palatschinken",
+		"src": "img/Palatschinken.jpg",
+		"description": "Ein einfaches Gericht, welches süß und herzhaft genossen werden kann.",
+		"ingredients": "250g Mehl;2 Stk. Eier;0,5 L Milch;1 Schuss Öl oder Butter;1 Prise Salz;gewünschte Toppings (Marmelade, Staubzucker, Wurst, Käse,...)",
+		"making": "Zuerst Mehl, Milch, Eier und Salz in eine Schüssel geben und glatt rühren. Öl/Butter in Pfanne erhitzen, einen Schöpfer Teig in das heiße Fett geben und die Palatschinke auf beiden Seiten goldbraun ausbacken. Mit beliebigen Zutaten füllen.",
+		"time": "30 min",
+		"gang": "Hauptspeise;Nachspeise;Frühstück"
+	}
 ]
 
 function RandomDish() {
